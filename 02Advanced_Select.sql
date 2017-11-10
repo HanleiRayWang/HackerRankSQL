775566ad7562369d592bae3a0af62e198844b53f
HanleiRayWang <raywang0703@gmail.com>
2017-11-10 11:57:46 -0800

    Update and rename Advanced_Select.sql to 02Advanced_Select.sql

--- 02Advanced_Select.sql
+++ 02Advanced_Select.sql
@@ -2,5 +2,5 @@
 select (case when A=B and B=C then 'Equilateral'
              when A+B<=C or B+C<=A or A+C<=B then 'Not A Triangle'
              when A=B or A=C or B=C then 'Isosceles'
-             when A<>B and B<>C then 'Scalene' end) as triangle
+             when A<>B and B<>C then 'Scalene' end)
 from TRIANGLES
